da966d50b51514511b97ebb600d926bc4e2bdc18
archit-aggarwal <archit.aggarwal023@gmail.com>
2020-07-14 11:27:20 +0530

    Updates

---
+++ precache-manifest.b08920d4b1f7f58d895760ab3bfa11dd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "019444d992d4602c8e6de550748cad89",
+    "url": "/react-hangman-marvel/index.html"
+  },
+  {
+    "revision": "7437b7f235c3099f3956",
+    "url": "/react-hangman-marvel/static/css/main.a144fda4.chunk.css"
+  },
+  {
+    "revision": "2ec7efc689b873b1092a",
+    "url": "/react-hangman-marvel/static/js/2.5f4e4275.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hangman-marvel/static/js/2.5f4e4275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7437b7f235c3099f3956",
+    "url": "/react-hangman-marvel/static/js/main.57f12810.chunk.js"
+  },
+  {
+    "revision": "b1184de62cb52803ffbc",
+    "url": "/react-hangman-marvel/static/js/runtime-main.346d9418.js"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/react-hangman-marvel/static/media/0.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/react-hangman-marvel/static/media/1.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/react-hangman-marvel/static/media/2.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/react-hangman-marvel/static/media/3.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/react-hangman-marvel/static/media/4.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/react-hangman-marvel/static/media/5.1b557aa7.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/react-hangman-marvel/static/media/6.c80394c2.jpg"
+  }
+]);
